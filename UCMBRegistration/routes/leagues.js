a406186d2be8b1d527aa213d598dcacad840bf78
Cori <corinne.a.ader@gmail.com>
2019-09-13 15:15:37 -0400

    index page converted to hbs

--- UCMBRegistration/routes/leagues.js
+++ UCMBRegistration/routes/leagues.js
@@ -2,6 +2,11 @@ var express = require("express");
 var router = express.Router();
 var fs = require('fs');
 
+// http://localhost:3000/leagues
+router.get('/', function(req, res, next) {
+    res.render('leagues', { title: 'Leagues' });
+});
+
 // GET http://localhost:3000/leagues/data
 router.get('/data', function(req, res, next) {
     try {
